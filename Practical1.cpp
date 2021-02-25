3b8a7a2ea2da06fe60bd3b428a01746bde4d4736
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2021-02-25 12:29:55 +0530

    Added Practical 1
    Wap to read and display mxn matrix

---
+++ Practical1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int read_matrix(int m,int n){
+    int mat[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+        }
+    }
+}
+int display_matrix(int m,int n){
+    int mat[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+int main(){
+    int m,n;
+    cout<<"Enter the no. of rows of matrix"<<" ";
+    cin>>m;
+    cout<<"Enter the no. of columns of matrix"<<" ";
+    cin>>n;
+    int mat[m][n];
+    read_matrix(m,n);
+    display_matrix(m,n);
+    return 0;
+}
