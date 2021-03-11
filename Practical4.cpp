09197e828b76cb27b83336fc91946f20f696b60d
JasleenKaur2000 <60056238+JasleenKaur2000@users.noreply.github.com>
2021-03-11 16:29:00 +0530

    Update Practical4.cpp

--- Practical4.cpp
+++ Practical4.cpp
@@ -6,14 +6,18 @@ float power(float a);
 void display(float data);
 int main()
 {
-    float a,d1,d2;
+   float a,d1,d2;
     int b;
-    cin>>"Enter base value: ">>a>>;
-    cin>>"Enter exponent value: ">>b;
+    cout<<"Enter base value: ";
+    cin>>a;
+    cout<<"Enter exponent value: ";
+    cin>>b;
     d1=power(a,b);
     d2=power(a);
-    cout<<"Result for power function with exponent provided as argument: "<<display(d1);
-    cout<<"Result for power function with exponent provided as default argument: "<<display(d2);
+    cout<<"Result for power function with exponent provided as argument: ";
+    display(d1);
+    cout<<"Result for power function with exponent as default argument(i.e. 2): ";
+    display(d2);
     return 0;
 } 
 float power(float a, int b){
