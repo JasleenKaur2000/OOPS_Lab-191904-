b230575ec441b4f0da6a502cc9b2488519c5308d
JasleenKaur2000 <60056238+JasleenKaur2000@users.noreply.github.com>
2021-05-21 12:15:52 +0530

    Wap to create a string class.
    WAP to create a string class to store, add, equate and display strings.

---
+++ Program6.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+const int MAX=20;
+
+class MyString{
+private:
+    char A[MAX];
+public:
+    MyString(){
+        //cout<<"default constructor"<<endl;
+    }
+    void read(){
+        cout<<"Enter a String: ";
+        cin.getline(A,MAX);
+    }
+    void show(){
+        cout<<A<<endl;
+    }
+    int length(){
+        int i=0;
+        for(i=0;A[i]!='\0';i++);
+        return i;
+    }
+    void reverse(){
+        int i=length();
+        char t;
+        for(int k=0,j=i-1;k<j;k++,j--){
+            t=A[j];A[j]=A[k];A[k]=t;
+        }
+        
+    }
+    bool equate(MyString &B){
+        int i=0;
+        for(i=0;A[i]!='\0';i++){
+            if(B.A[i]!=A[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void add(MyString &B){
+        MyString temp;
+        int i=0;
+        cout<<"New string is: ";
+        for(i=0;A[i]!='\0';i++){
+            temp.A[i]=A[i];
+            cout<<temp.A[i];
+        }
+        cout<<" ";
+        for(int j=0;j<B.length();j++){
+            temp.A[j]=B.A[j];
+            cout<<temp.A[j];
+        }        
+    }
+    
+};
+
+int main(){
+    MyString s,r;
+    s.read();
+    s.show();
+    r.read();
+    r.show();
+    if(s.equate(r)){
+        cout<<"Strings are equal"<<endl;
+    }else{
+        cout<<"Strings are not equal"<<endl;
+    }
+    
+    s.add(r);
+    
+    return 0;
+   
+}
